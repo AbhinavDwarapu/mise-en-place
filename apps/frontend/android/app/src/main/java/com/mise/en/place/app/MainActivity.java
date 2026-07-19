a05a7ecbbabe75e54f0470514bfeeadadbf37996
AbhinavDwarapu <abhinavdwarapu+github@gmail.com>
2026-07-19 13:56:19 +0100

    feat: add capacitor to the frontend app

---
+++ apps/frontend/android/app/src/main/java/com/mise/en/place/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mise.en.place.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mise.en.place.app',
+  appName: 'Mise En Place',
+  webDir: 'dist'
+};
+
+export default config;
