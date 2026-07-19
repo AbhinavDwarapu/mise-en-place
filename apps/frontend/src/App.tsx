53d0000afa201f5ed7efd37ce21eef4e55552a3b
AbhinavDwarapu <abhinavdwarapu+github@gmail.com>
2026-07-19 14:58:01 +0100

    feat: add ionic/react router package and setup

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { IonContent, IonPage } from '@ionic/react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -8,8 +9,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="center">
+    <IonPage>
+      <IonContent>
+      <section id="center" className='bg-black'>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -115,7 +117,8 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
-    </>
+      </IonContent>
+    </IonPage>
   )
 }
 
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,22 @@
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Route } from 'react-router-dom'
+import '@ionic/react/css/core.css'
 import './index.css'
 import App from './App.tsx'
 
+setupIonicReact()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/" component={App} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
   </StrictMode>,
 )
