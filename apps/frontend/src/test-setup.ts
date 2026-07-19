5fa297944a5e6229353bfaeb2354dc9e84b6055a
AbhinavDwarapu <abhinavdwarapu+github@gmail.com>
2026-07-19 19:49:52 +0100

    test: add e2e tests for kitchen ingredients

--- apps/frontend/src/test-setup.ts
+++ apps/frontend/src/test-setup.ts
@@ -1,1 +1,24 @@
 import '@testing-library/jest-dom/vitest'
+
+globalThis.ResizeObserver ??= class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.matchMedia ??= ((query: string) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: () => {},
+  removeListener: () => {},
+  addEventListener: () => {},
+  removeEventListener: () => {},
+  dispatchEvent: () => false,
+})) as typeof window.matchMedia
+
+Element.prototype.scrollIntoView ??= () => {}
+Element.prototype.hasPointerCapture ??= () => false
+Element.prototype.setPointerCapture ??= () => {}
+Element.prototype.releasePointerCapture ??= () => {}
+Element.prototype.getAnimations ??= () => []
