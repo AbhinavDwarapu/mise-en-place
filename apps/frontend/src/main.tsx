54f7c84e42887ae70f1abfed9b06629d3b0b7b7d
AbhinavDwarapu <abhinavdwarapu+github@gmail.com>
2026-07-19 19:02:52 +0100

    feat: add kitchen route

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,10 +2,10 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import '@ionic/react/css/core.css'
 import './index.css'
-import App from './App.tsx'
+import { KitchenPage } from '@/routes/kitchen-page'
 
 setupIonicReact()
 
@@ -14,7 +14,8 @@ createRoot(document.getElementById('root')!).render(
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          <Route exact path="/" component={App} />
+          <Route exact path="/kitchen" component={KitchenPage} />
+          <Redirect exact from="/" to="/kitchen" />
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
